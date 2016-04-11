99e2ca993f3239e57e46fb316e4e11fbdf372208
Maxim Kornilov <maxim.korneelov@gmail.com>
2016-04-11 23:38:34 +0300

    ICM-86 Made that only archive for vertx-guice library to be deployed. ICM-86 "Extend vertx-guice to use single injector for all verticles".

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,8 +4,8 @@ set -e
 
 if [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying release version ${TRAVIS_TAG}"
-    ./gradlew uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="local.secring.gpg" -PmavenVersion="${TRAVIS_TAG}"
+    ./gradlew :vertx-guice:uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="local.secring.gpg" -PmavenVersion="${TRAVIS_TAG}"
 else
     echo "Deploying snapshot version"
-    ./gradlew uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="local.secring.gpg"
+    ./gradlew :vertx-guice:uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="local.secring.gpg"
 fi
